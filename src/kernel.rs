aa2ea3c3302774cf7e281b78e75d75a096be47bd
Nathael Bonnal <kirua48000@gmail.com>
2023-05-16 16:41:52 +0200

    :construction: create kernel.rs

---
+++ src/kernel.rs
@@ -0,0 +1,78 @@
+use std::any::{Any, type_name, TypeId};
+use std::collections::HashMap;
+use std::fmt::Pointer;
+use std::iter::Flatten;
+use std::ops::Deref;
+use crate::server::Server;
+
+pub trait Service: AsAny + AsAnyMut {
+  fn start(&self);
+}
+
+pub trait AsAnyMut {
+  fn as_any_mut(&mut self) -> &mut dyn Any;
+}
+
+impl<T: Any> AsAnyMut for T {
+  fn as_any_mut(&mut self) -> &mut dyn Any {
+    self
+  }
+}
+
+pub trait AsAny {
+  fn as_any(&self) -> &dyn Any;
+}
+
+impl<T: Any> AsAny for T {
+  fn as_any(&self) -> &dyn Any {
+    self
+  }
+}
+
+pub struct Kernel {
+  services: HashMap<TypeId, Box<dyn Service>>,
+}
+
+impl Kernel {
+  pub fn new() -> Self {
+    Kernel {
+      services: HashMap::new(),
+    }
+  }
+
+  pub fn register_service<T>(&mut self, service: T)
+    where
+      T: 'static + Service,
+  {
+    //println!("Enregistrement du service de type {:?}", TypeId::of::<T>());
+    let router_ptr = std::ptr::addr_of!(service);
+    println!("Adresse mémoire de router: {:p}", router_ptr);
+    self.services.insert(TypeId::of::<T>(), Box::new(service));
+  }
+
+  pub fn get_service<T>(&self) -> Option<&T>
+    where
+      T: 'static + Service,
+  {
+    self.services.get(&TypeId::of::<T>())
+      .map(|service| {
+        let service_any = service.as_any();
+        service_any.downcast_ref::<T>().unwrap()
+      })
+  }
+
+  pub fn get_service_mut<T: 'static>(&mut self) -> Option<&mut T> {
+    if let Some(service) = self.services.get_mut(&TypeId::of::<T>()) {
+      service.as_mut().as_any_mut().downcast_mut::<T>()
+    } else {
+      None
+    }
+  }
+
+  pub fn boot (&mut self) {
+    for (type_id, service) in &mut self.services {
+      service.start();
+
+    }
+  }
+}
