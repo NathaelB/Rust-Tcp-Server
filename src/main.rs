53c87130528cdf1dbb8797a8cc61e85f3e43cc47
Nathael Bonnal <kirua48000@gmail.com>
2023-05-17 13:52:55 +0200

    :construction: refractor

--- src/main.rs
+++ src/main.rs
@@ -5,31 +5,14 @@ mod database;
 use std::io::Write;
 use std::net::Shutdown;
 use server::Server;
+use crate::router::Router;
 
 fn main () {
-  let server = Server::new("127.0.0.1:3333", 10);
+  let router = Router::new()
+    .add_route("/hello", |stream| {
+      stream.write(b"Hello, World!").unwrap();
+      stream.flush().unwrap();
+    }).unwrap();
 
-  match server {
-    Ok(mut server) => {
-      server.router.add_route("/hello", |stream| {
-        stream.write(b"Hello, World!").unwrap();
-        stream.flush().unwrap();
-      });
-
-      server.router.add_route("/algebre", |stream| {
-        stream.write(b"L'algebre ton pire cauchemar!").unwrap();
-        stream.flush().unwrap();
-      });
-
-      server.router.add_route("/end", |stream| {
-        println!("DISCONECTED CONNEXION");
-        stream.shutdown(Shutdown::Both).unwrap();
-      });
-
-      server.run();
-    }
-    Err(e) => {
-      println!("Error lors du démarrage: {}", e);
-    }
-  };
+  Server::new(("127.0.0.1", 3333), 10, router).run();
 }
